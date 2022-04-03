a576fbbdf0c3cc6678949b445e3f15a516367250
Thomas Ricci <git@thomasricci.dev>
2022-04-03 03:33:33 -0400

    some planning for fifth test

--- create-octobox-app/test/five.test.js
+++ create-octobox-app/test/five.test.js
@@ -2,7 +2,11 @@ const utils = require("./testutils.js");
 const {execSync, exec: execAsync} = require("child_process");
 const fs = require("fs");
 
-// TODO: we're probably gonna do this by making some apps with eslint/stylelint problems and running both linters scripts to make sure they fail. we'll be taking care of vite integration in test 2 by comparing hashes, so we shouldnt need to connect to the vite server and somehow try to see if an error was rendered on the screen.
+// TODO: we're probably gonna do this by making some apps with eslint/stylelint problems and running both linters scripts to make sure they have nonzero exit codes. we'll be taking care of vite integration in test 2 by comparing hashes, so we don't need to worry about that
+
+// use these commands:
+// eslint "**/*.{js,ts,jsx,tsx}"
+// npx -y stylelint "**/*.scss"
 
 const test = async () => {
   // this test makes apps with linters and tests them to make sure they lint
