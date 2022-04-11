b329ace35303ec56556466df03c3e53f612908c4
Thomas Ricci <git@thomasricci.dev>
2022-04-10 22:29:04 -0400

    minify octobox-utils

--- octobox-utils/rollup.config.js
+++ octobox-utils/rollup.config.js
@@ -2,6 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require("./package.json");
@@ -22,9 +24,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
     ],
   },
   {
