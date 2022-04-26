b8aca60fc8b118d01c997b4862a9176798065e47
Thomas Ricci <git@thomasricci.dev>
2022-04-25 22:54:31 -0400

    more typescript bugfixing

--- create-octobox-app/src/main.ts
+++ create-octobox-app/src/main.ts
@@ -169,6 +169,7 @@ import ReactDOM from "react-dom/client";
 import "./styles/main.scss";
 import { App } from "./App";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App/>
