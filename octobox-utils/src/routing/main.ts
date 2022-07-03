4f6ddd4eab7fc6c41449c0a438970e8124f87972
Thomas Ricci <git@thomasricci.dev>
2022-07-02 22:13:10 -0400

    Add outlet export since its so commonly used

--- octobox-utils/src/routing/main.ts
+++ octobox-utils/src/routing/main.ts
@@ -1,9 +1,15 @@
 import * as RL from "@tanstack/react-location";
+import { FunctionComponent } from "react";
 
 /**
  * The bundled ReactLocation library.
  */
 const ReactLocation = RL;
+
+/**
+ * An outlet is a React component which will render a child route. For example, if your URL was <a href="https://example.com/cart/checkout">https://example.com/cart/checkout</a>, and you rendered an <code>&#60;Outlet/&#62;</code> in your cart component, your checkout component would be rendered inside that <code>&#60;Outlet/&#62;</code>.
+ */
+const Outlet: FunctionComponent = RL.Outlet;
 
 export * from "./api/Anchor";
 export * from "./api/AnchorChildren";
@@ -20,4 +26,4 @@ export * from "./api/useParams";
 export * from "./api/useRedirect";
 export * from "./api/useReload";
 export * from "./api/useRoutingInternals";
-export { ReactLocation };
+export { ReactLocation, Outlet };
