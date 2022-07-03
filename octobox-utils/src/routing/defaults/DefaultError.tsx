2cad373449b85be692c3079f652899c9add9e69a
Thomas Ricci <git@thomasricci.dev>
2022-07-03 00:05:51 -0400

    add new pend and err elements

--- octobox-utils/src/routing/defaults/DefaultError.tsx
+++ octobox-utils/src/routing/defaults/DefaultError.tsx
@@ -1,9 +1,14 @@
 import React, { FC, ReactElement } from "react";
+import "./style.css";
 
 export const DefaultError: FC = (): ReactElement => {
   return (
     <React.Fragment>
-      <p>An unknown error occoured!</p>
+      <div className={"err-c"}>
+        <div className={"err"}>
+          <p>An unknown error occoured!</p>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
