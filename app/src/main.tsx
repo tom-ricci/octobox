237b17058d087c952c0ed6d70b6d49e9f91b267c
Thomas Ricci <git@thomasricci.dev>
2022-11-25 01:05:18 -0500

    update testbench app

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./styles/main.scss";
 import { App } from "./App";
 
+for(const elem of document.head.querySelectorAll("[react=true]")) elem.setAttribute("prerender", "true");
+(document.body.children[0] as HTMLElement).style.display = "block";
+document.getElementById("root")?.children[0].remove();
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<div><React.StrictMode><App/></React.StrictMode></div>);
